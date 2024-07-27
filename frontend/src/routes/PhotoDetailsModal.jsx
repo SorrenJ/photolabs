52d71e69141a1c2bf022818f6b9b7935efb3c22f
SorrenJ <soalexjao@gmail.com>
2024-07-26 18:45:35 -0700

    pass photo details to PhotoDetailsModal, and similar_photos details, console.log the data

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,14 +3,28 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ onClose }) => {
+const PhotoDetailsModal = ({photo, onClose}) => {
+  console.log("User name: "+ photo.user.name);
+  console.log("User city: "+ photo.location.city);
+  console.log("User country: "+ photo.location.country);
+  
+
+  const similarPhotos = photo.similar_photos;
+  console.log("Similar photo 2 ID: " + similarPhotos.photo2.id);
+  console.log("Similar photo 3 ID: " + similarPhotos.photo3.id);
+  console.log("Similar photo 4 ID: " + similarPhotos.photo4.id);
+  console.log("Similar photo 5 ID: " + similarPhotos.photo5.id);
+ 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+  
     </div>
-  )
+    
+  );
+
 };
 
 export default PhotoDetailsModal;
